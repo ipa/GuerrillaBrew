8d300ae2f406392bcf7a3465da87b615a4b90dbf
Iwan Paolucci <iwan.paolucci@gmail.com>
2016-04-13 21:33:24 +0200

    fixed config parser

--- src/FermentationControl/fermentationcontrol/util/Config.py
+++ src/FermentationControl/fermentationcontrol/util/Config.py
@@ -10,14 +10,14 @@ class Config(Borg):
         Borg.__init__(self)
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
-        self.use_config = self.config['DEFAULT']['UseConfig']
+        self.use_config = 'DEVELOP'#self.config['DEFAULT']['UseConfig']
 
     def get_sensors(self):
-        sensors = ast.literal_eval(self.config[self.use_config]['Sensors'])
+        sensors = ast.literal_eval(self.config.get(self.use_config, 'Sensors'))
         return sensors
 
     def get_sensor_interval(self):
-        return ast.literal_eval(self.config[self.use_config]['SensorInterval'])
+        return ast.literal_eval(self.config.get(self.use_config, 'SensorInterval'))
 
     def get_display_sensor(self):
-        return self.config[self.use_config]['DisplaySensor']
+        return self.config.get(self.use_config, 'DisplaySensor')
