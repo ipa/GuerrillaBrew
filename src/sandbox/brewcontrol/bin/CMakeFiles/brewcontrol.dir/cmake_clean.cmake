68374883748b9c968551a80a7777ffdaac2e13d1
Iwan Paolucci <iwan.paolucci@gmail.com>
2016-04-22 22:25:12 +0200

    moved stuff to sandbox folder

---
+++ src/sandbox/brewcontrol/bin/CMakeFiles/brewcontrol.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/brewcontrol.dir/main.cpp.o"
+  "brewcontrol.pdb"
+  "brewcontrol"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/brewcontrol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
